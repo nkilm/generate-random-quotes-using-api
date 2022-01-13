1c327aa9aed46a35a406d98202e32c7b398e817b
nkilm <nikhilmohitelhs@gmail.com>
2022-01-13 15:04:42 +0530

    Added Error handling for fetch

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const URL = "http://api.quotable.io/random";
 
 
 fetch(URL)
-    .then(res => res.json())
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            throw new Error("Something wrong with Response");
+        }
+    })
     .then(info => {
         console.log(info); // contains random quote from the api
+    })
+    .catch(error=>{
+        console.log(error);
     })
 
 
