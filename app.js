28e0bd34eb427bd3a475a25c886e8679ab472d67
nkilm <nikhilmohitelhs@gmail.com>
2022-01-13 16:52:54 +0530

    adding functionality to button

--- app.js
+++ app.js
@@ -38,4 +38,14 @@ getQuote().then((quoteObj) => {
     console.log(error);
 })
 
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click",()=>{
+    getQuote().then((quoteObj) => {
+        quote.textContent = `"${quoteObj.content}"`
+        author.textContent = `- ${quoteObj.author}`
+    }).catch((error) => {
+        console.log(error);
+    })
+})
 
