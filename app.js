1a25366c0c26f0256653c18d25feb777664b3a95
nkilm <nikhilmohitelhs@gmail.com>
2022-01-13 16:49:05 +0530

    define function using async and await to reduce the code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const URL = "http://api.quotable.io/random";
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 
+/* 
 fetch(URL)
     .then(res => {
         if (res.ok) {
@@ -17,7 +18,24 @@ fetch(URL)
     })
     .catch(error => {
         console.log(error);
-    })
+    }) 
 
+*/
+
+async function getQuote() {
+    const response = await fetch(URL);
+    if (!response.ok) {
+        throw new Error("Something Wrong With Response");
+    }
+    const data = await response.json();
+    return data;
+}
+
+getQuote().then((quoteObj) => {
+    quote.textContent = `"${quoteObj.content}"`
+    author.textContent = `- ${quoteObj.author}`
+}).catch((error) => {
+    console.log(error);
+})
 
 
